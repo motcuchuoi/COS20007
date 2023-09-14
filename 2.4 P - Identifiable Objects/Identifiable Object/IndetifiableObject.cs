8980b945def99afa35c522fe0c5b3c00804baaa0
motcuchuoi <144986342+motcuchuoi@users.noreply.github.com>
2023-09-14 16:09:09 +1000

    Add files via upload

---
+++ 2.4 P - Identifiable Objects/Identifiable Object/IndetifiableObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventure
+{
+    public class IdentifiableObject
+    {
+        List<string> _identifiers = new List<string>(); // create a new list named identifiers
+        public IdentifiableObject(string[] idents) 
+        {   
+            foreach(string id in idents)
+            {
+                _identifiers.Add(id.ToLower());
+            }
+        }
+        public bool AreYou(string id)
+        {
+            return _identifiers.Contains(id.ToLower());
+        }
+        public string FirstId
+        {
+            get
+            {
+                if (_identifiers.Count == 0)
+                {
+                    return "";
+                }
+                else
+                {
+                    return _identifiers[0];
+                }
+            }
+        }
+        public void AddIdentifier(string id)
+        {
+            _identifiers.Add(id.ToLower());
+        }
+    }
+}
