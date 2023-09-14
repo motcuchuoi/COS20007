05ff43f13f3e324a46538f9c8ec5d6a34b0e5d96
motcuchuoi <144986342+motcuchuoi@users.noreply.github.com>
2023-09-14 16:17:52 +1000

    Create Program.cs

---
+++ 3.1P - Clock Class/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ClockClass
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Clock clock = new Clock();
+      for (int i = 0; i < 120; i++)
+      {
+        clock.Ticks();
+        Console.WriteLine(clock.GetTime);
+        Thread.Sleep(1000);
+        Console.Clear();
+      }
+    }
+  }
+}
